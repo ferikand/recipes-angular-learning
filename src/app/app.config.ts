cc790ea15a95ee9485701f4d8a14c0b1980a18c0
Andrii Ferenchuk <ferenchuk.a@hotmail.com>
2026-01-12 19:35:56 +0200

    module 3 done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,5 @@
-import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
-import {provideRouter, withComponentInputBinding} from '@angular/router';
-
-import {routes} from './app.routes';
-import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
+import { ApplicationConfig } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes, withComponentInputBinding()), provideClientHydration(withEventReplay())
-  ]
+  providers: []
 };
