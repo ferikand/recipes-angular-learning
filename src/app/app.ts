cb5afb7c3ff7b19da018047d58fb3f6e03676408
Andrii Ferenchuk <ferenchuk.a@hotmail.com>
2026-01-12 21:40:14 +0200

    add task list and update counter to reflect tasks length

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,4 +10,22 @@ import {CounterComponent} from './CounterComponent'
 })
 export class App {
     title = 'Recipes'
+    tasks = [
+        {
+            id: 1,
+            title: 'learn angular',
+        },
+        {
+            id: 2,
+            title: 'learn node',
+        },
+        {
+            id: 3,
+            title: 'learn react',
+        },
+        {
+            id: 4,
+            title: 'learn java',
+        },
+    ]
 }
